c210cbbb0c722e68a189b030237a8d87cee79d07
https://陈磊.中国 <flycl@vip.qq.com>
2018-10-01 21:37:22 +0800

    创建目录

---
+++ Git/LearnJava.java
@@ -0,0 +1,12 @@
+public class LearnJava{
+
+	//主方法 入口函数 在
+	public static void main(String[] args) {
+		System.out.println("系统输出");
+	}
+
+
+	//添加一个说明
+
+
+}
---
+++ Git/LearnJava2.java
@@ -0,0 +1,14 @@
+//静态方法
+private class Anmin{
+	
+	
+}
+
+public class LearnJava2{
+
+	//主方法 入口函数 在
+	public static void main(String[] args) {
+		System.out.println("系统输出");
+	}
+}
+
