b94f4fec18f0ac8c721321575f8c74ec386c4151
https://陈磊.中国 <flycl@vip.qq.com>
2018-10-03 17:46:09 +0800

    更新全部文件到版本库

---
+++ Git/JavaLearn.java
@@ -0,0 +1,17 @@
+public class LearnJava{
+
+	//主方法 入口函数
+	public class test{
+
+	}
+	
+	// 添加注解
+	public static void main(String[] args) {
+		System.out.println("系统输出");
+	}
+
+	添加一个说明
+	//添加一个说明
+
+
+}
